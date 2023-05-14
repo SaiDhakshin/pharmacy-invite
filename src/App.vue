c0bf36f1e80d7f9e738dc2503e2fd317ab50d25d
Mariyon Sunil Larsen J <mariyonsunillarsenj@Mariyons-MacBook-Air.local>
2023-05-14 12:15:59 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="invitation">
+      <img class="logo" src="@/assets/plus.jpeg" alt="" />
+      <p class="text">Kerala Private Pharmacists Association</p>
+
+      <h1 class="text heading">
+        "Join Us for Membership Day: Kerala Private Pharmacists Association"
+      </h1>
+      <div class="banner-container">
+        <img class="banner" src="@/assets/banner-1.jpg" alt="" />
+        <img class="banner" src="@/assets/banner-2.jpg" alt="" />
+      </div>
+
+      <ul>
+        <li class="text heading">Avenue: Cheth Auditorium, Vadakara</li>
+        <li class="text heading">Date: 17th May 2023, Wednesday</li>
+        <li class="text heading">Time: 10 a.m</li>
+      </ul>
+
+      <p class="text heading">
+        We look forward to seeing you at our membership day and sharing
+        knowledge and ideas with fellow pharmacists in Kerala
+      </p>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped></style>
